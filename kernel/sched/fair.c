2d168aa0ffbce2790cd7d620c9a2225bd4d4fe75
suokun <suokun24@126.com>
2015-04-06 11:15:36 -0600

    modified:   kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -825,6 +825,7 @@ update_stats_curr_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	//add by Kun
 	struct vcpu_runstate_info *state;
 
+
 	state = &__get_cpu_var(xen_runstate);
 	se->running_time = state->time[RUNSTATE_running];
 	se->runnable_time = state->time[RUNSTATE_runnable];
